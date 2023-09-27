6ad599350e9d0d47262a781bec4f1cf8e85db403
Dishant Kumar Kushwaha <dishantkushwaha71@gmail.com>
2023-09-28 00:57:44 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+  {
+    name: 'Dishant',
+    email: 'dishantkushwaha@gmail.com',
+    id: 1,
+  },
+  {
+    name: 'Himansh',
+    email: 'himanshmalhotra@gmail.com',
+    id: 2,
+  },
+  {
+    name: 'Ajay',
+    email: 'ajaysagar@gmail.com',
+    id: 3,
+  },
+]
